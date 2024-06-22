8ee3417889852c1d374a43d106ee93264e260287
Leonel-Ventura <112353107+Leonel-Ventura@users.noreply.github.com>
2024-06-23 00:41:44 +0100

    Update pesquisa.jsx

--- src/routes/pesquisa.jsx
+++ src/routes/pesquisa.jsx
@@ -27,13 +27,13 @@ export default function Pesquisa() {
 
     return (
         <div>
-            <div id="first" className="bg-[#E63946] flex flex-col justify-between mb-4 sm:flex-row">
+            <div id="first" className="bg-[#E63946] flex flex-col justify-between mb-4 sm:flex-row py-2">
                <h1 className=" font-bold text-white p-5
                 text-3xl">
                     <Link to={"/"}>Cine<span className="text-slate-950">Click</span></Link>
                 </h1>
                 
-                <button type="submit" className="text-white self-center bg-slate-950 mx-4 p-2 rounded-lg">
+                <button type="submit" className="text-white self-center bg-slate-950 mx-4 p-4 rounded-lg">
                     <Link to={"/"}>Voltar</Link>
                 </button>
             </div>
