a5b26ad09354c365ec3c014f675fa05282b490c3
Leonel-Ventura <leonelcutendana@gmail.com>
2024-06-25 15:58:38 +0100

    last changes

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,7 +3,7 @@ export function NotFound () {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded shadow-md max-w-sm w-full">
         <h2 className="text-2xl font-bold mb-4 text-gray-800">404 - Filme Não Encontrado.</h2>
-        <p className="text-gray-600">APor favor escreva correctamente o nome do filme.</p>
+        <p className="text-gray-600">Por favor escreva correctamente o nome do filme.</p>
       </div>
     </div>
   );
